cd7b48f5a01510e966b168878edeb323fbc65426
roux george buciu <rouxbuciu@users.noreply.github.com>
2019-01-20 11:10:07 -0500

    Update downloads.exclude.sh

--- downloads.exclude.sh
+++ downloads.exclude.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Nothing to download yet."
+# Install oh-my-zsh framework
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Get POWERLEVER9K
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Get ZSH syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Get ZSH auto-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# VIM PLUG!
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
