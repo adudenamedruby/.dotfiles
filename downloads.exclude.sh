985746203428458eeb756a7b17482525d1976f0b
roux george buciu <11182210+iwillbeshera@users.noreply.github.com>
2020-07-23 16:40:06 -0400

    Update downloads.exclude.sh

--- downloads.exclude.sh
+++ downloads.exclude.sh
@@ -2,16 +2,14 @@
 
 # Install oh-my-zsh framework
 cd ~
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd ~/.dotfiles
 
-# Get POWERLEVER9K
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# Get POWERLEVER10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # Get ZSH syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+brew install zsh-syntax-highlighting
 
 # Get ZSH auto-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-
