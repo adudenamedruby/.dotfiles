f0d1934cf2133972cdff695c60452965c18db873
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2021-01-20 09:30:02 -0500

    Update downloads.exclude.sh

--- downloads.exclude.sh
+++ downloads.exclude.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Get XVim
+git clone https://github.com/XVimProject/XVim2.git ~/Xvim
 
 # Get POWERLEVER10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
