6dbd9252ddb18d0f97347a95ef9e8468cadb0b16
roux g. buciu <11182210+electricRGB@users.noreply.github.com>
2021-01-24 10:48:30 -0500

    Update downloads.exclude.sh

--- downloads.exclude.sh
+++ downloads.exclude.sh
@@ -2,11 +2,3 @@
 
 # Get XVim
 git clone https://github.com/XVimProject/XVim2.git ~/Xvim
-
-# Get POWERLEVER10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
-
-# Install oh-my-zsh framework
-cd ~
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd ~/.dotfiles
