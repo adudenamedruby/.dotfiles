05a4e762f428fe9e84b0b661e604cda8eb3e2e36
roux g. buciu <github@fringe.foundation>
2023-07-21 18:38:40 -0400

    add more stuff I don't understand

---
+++ emacs/.emacs.d/synthmacs/modules/languages/synthmacs-lisp.el
@@ -0,0 +1,9 @@
+(use-package sly)
+
+(use-package sly-macrostep)
+
+(use-package sly-repl-ansi-color
+  :init
+  (add-to-list 'sly-contribs 'sly-repl-ansi-color))
+
+(provide 'synthmacs-lisp)
---
+++ emacs/.emacs.d/synthmacs/packages/synthmacs-snippets.el
@@ -0,0 +1,9 @@
+(use-package yasnippet)
+
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
+
+(use-package yasnippet-snippets)
+(use-package common-lisp-snippets)
+
+(provide 'synthmacs-snippets)
