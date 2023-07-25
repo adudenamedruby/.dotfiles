468899b85186fcc0df37e4d7a0f36af43575e6dd
roux g. buciu <github@fringe.foundation>
2023-07-25 09:39:40 -0400

    updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -98,3 +98,20 @@
 
   ;;; init.el ends here
 ;; init.el: load modules:1 ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+	   (lambda nil
+	     (progn
+	       (synthmacs/tangle-config)))
+	   nil t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
