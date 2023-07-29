1f6f4d5cf90ca677c9894970afc0530ddfe5f488
roux g. buciu <github@fringe.foundation>
2023-07-28 20:14:27 -0400

    Add init files?

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,55 @@
+;;; init.el --- Personal configuration file -*- lexical-binding: t; no-byte-compile: t; -*-
+;; NOTE: init.el is now generated from synthmacs.org.  Please edit that file instead
+
+;; `file-name-handler-alist' is consulted on every `require', `load' and various
+;; path/io functions. You get a minor speed up by nooping this. However, this
+;; may cause problems on builds of Emacs where its site lisp files aren't
+;; byte-compiled and we're forced to load the *.el.gz files (e.g. on Alpine)
+(unless (daemonp)
+  (defvar doom--initial-file-name-handler-alist file-name-handler-alist)
+  (setq file-name-handler-alist nil)
+  ;; Restore `file-name-handler-alist' later, because it is needed for handling
+  ;; encrypted or compressed files, among other things.
+  (defun doom-reset-file-handler-alist-h ()
+    ;; Re-add rather than `setq', because changes to `file-name-handler-alist'
+    ;; since startup ought to be preserved.
+    (dolist (handler file-name-handler-alist)
+      (add-to-list 'doom--initial-file-name-handler-alist handler))
+    (setq file-name-handler-alist doom--initial-file-name-handler-alist))
+  (add-hook 'emacs-startup-hook #'doom-reset-file-handler-alist-h)
+  (add-hook 'after-init-hook '(lambda ()
+				;; restore after startup
+				(setq gc-cons-threshold 16777216
+				      gc-cons-percentage 0.1)))
+  )
+
+;; Ensure Synthmacs is running out of this file's directory
+(setq user-emacs-directory (file-truename (file-name-directory load-file-name)))
+
+(message "SynthMacs is powering up, please be patient...")
+
+;; (add-to-list 'load-path "~/.emacs.d/synthmacs/")
+(add-to-list 'load-path (expand-file-name "synthmacs" user-emacs-directory))
+
+(let ((file-name-handler-alist nil)
+      (gc-cons-threshold 100000000))
+
+  (require 'synthmacs-core)
+  (require 'synthmacs-package-management)
+  (require 'synthmacs-general-evil)
+  (require 'synthmacs-ui)
+  (require 'synthmacs-org)
+  (require 'synthmacs-completion-framework)
+  (require 'synthmacs-tools)
+  (require 'synthmacs-programming)
+  ;;(require 'synthmacs-lang-clojure)
+  ;;(require 'synthmacs-lang-haskell)
+  ;;(require 'synthmacs-lang-html)
+  ;;(require 'synthmacs-lang-lisp)
+  ;;(require 'synthmacs-lang-markdown)
+  ;;(require 'synthmacs-lang-rust)
+  ;;(require 'synthmacs-lang-swift)
+  ;;(require 'synthmacs-lang-yaml)
+  )
+
+;;; init.el ends here
