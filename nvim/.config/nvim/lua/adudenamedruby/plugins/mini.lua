6658d3480fc9040a42c73560d42580259aaf2b33
roux g. buciu <github@adudenamedruby.com>
2024-09-20 18:30:40 -0400

    Testing a commit

--- nvim/.config/nvim/lua/adudenamedruby/plugins/mini.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/mini.lua
@@ -18,8 +18,6 @@ return {
 		require("mini.surround").setup()
 
 		-- Simple and easy statusline.
-		--  You could remove this setup call if you don't like it,
-		--  and try some other statusline plugin
 		local statusline = require("mini.statusline")
 		-- set use_icons to true if you have a Nerd Font
 		statusline.setup({ use_icons = vim.g.have_nerd_font })
