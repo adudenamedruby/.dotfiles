d461bcc0d2147ecfecfdf36106ce51155a74d572
roux g. buciu <github@adudenamedruby.com>
2024-09-21 17:23:57 -0400

    Update whichkey ui

--- nvim/.config/nvim/lua/adudenamedruby/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/whichkey.lua
@@ -3,6 +3,7 @@ return {
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
+			preset = "modern",
 			icons = {
 				-- set icon mappings to true if you have a Nerd Font
 				mappings = vim.g.have_nerd_font,
