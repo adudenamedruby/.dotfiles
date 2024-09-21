c586a1a21a72f98977f9120193d294641bbd8327
roux g. buciu <github@adudenamedruby.com>
2024-09-21 07:33:41 -0400

    Starting to setup leader

--- nvim/.config/nvim/lua/adudenamedruby/core/keymaps.lua
+++ nvim/.config/nvim/lua/adudenamedruby/core/keymaps.lua
@@ -1,39 +1,72 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<leader>sc", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+--close NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Buffer menu
+vim.keymap.set("n", "<leader><TAB>", "<cmd>:b#<CR>", { desc = "switch to last buffer" })
+vim.keymap.set("n", "<leader>be", "<cmd>:enew<CR>", { desc = "open empty buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>:bd<CR>", { desc = "delete buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>:bn<CR>", { desc = "next buffer" })
+vim.keymap.set("n", "<leader>bp", "<cmd>:bp<CR>", { desc = "previous buffer" })
+
+-- Debug menu
+
+-- Error menu
+
+-- Files menu
+vim.keymap.set("n", "<leader>fs", "<cmd>w<CR>", { desc = "[S]ave file" })
+
+-- Git menu
+vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame<CR>", { desc = "git blame" })
+vim.keymap.set("n", "<leader>gc", "<cmd>Gitsigns blame_line<CR>", { desc = "git blame line" })
+
+-- Help menu
+
+-- Quit menu
+vim.keymap.set("n", "<leader>qq", "<cmd>q<CR>", { desc = "[Q]uit nVim" })
+
+-- Registers menu
+
+-- Search menu
+
+-- Toggle menu
+
+-- Windows menu
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
---
+
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<leader>wh', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<leader>wl', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<leader>wj', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<leader>wk', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<leader>wh", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<leader>wl", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<leader>wj", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<leader>wk", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<leader>ws", "<cmd>:split<CR>", { desc = "horizontal split" })
+vim.keymap.set("n", "<leader>wv", "<cmd>:vsplit<CR>", { desc = "vertical split" })
 
-vim.keymap.set('n', '<leader>qq', '<cmd>q<CR>', { desc = '[Q]uit nVim' })
-vim.keymap.set('n', '<leader>fs', '<cmd>w<CR>', { desc = '[S]ave file' })
+vim.keymap.set("n", "<leader>wd", "<cmd>:q<CR>", { desc = "delete current window" })
+vim.keymap.set("n", "<leader>wo", "<cmd>:only<CR>", { desc = "close all windows except current" })
--- nvim/.config/nvim/lua/adudenamedruby/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/whichkey.lua
@@ -1,55 +1,60 @@
 return {
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
-    opts = {
-      icons = {
-        -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
-        -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-        -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
-        keys = vim.g.have_nerd_font and {} or {
-          Up = '<Up> ',
-          Down = '<Down> ',
-          Left = '<Left> ',
-          Right = '<Right> ',
-          C = '<C-…> ',
-          M = '<M-…> ',
-          D = '<D-…> ',
-          S = '<S-…> ',
-          CR = '<CR> ',
-          Esc = '<Esc> ',
-          ScrollWheelDown = '<ScrollWheelDown> ',
-          ScrollWheelUp = '<ScrollWheelUp> ',
-          NL = '<NL> ',
-          BS = '<BS> ',
-          Space = '<Space> ',
-          Tab = '<Tab> ',
-          F1 = '<F1>',
-          F2 = '<F2>',
-          F3 = '<F3>',
-          F4 = '<F4>',
-          F5 = '<F5>',
-          F6 = '<F6>',
-          F7 = '<F7>',
-          F8 = '<F8>',
-          F9 = '<F9>',
-          F10 = '<F10>',
-          F11 = '<F11>',
-          F12 = '<F12>',
-        },
-      },
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			icons = {
+				-- set icon mappings to true if you have a Nerd Font
+				mappings = vim.g.have_nerd_font,
+				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+				-- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
+				keys = vim.g.have_nerd_font and {} or {
+					Up = "<Up> ",
+					Down = "<Down> ",
+					Left = "<Left> ",
+					Right = "<Right> ",
+					C = "<C-…> ",
+					M = "<M-…> ",
+					D = "<D-…> ",
+					S = "<S-…> ",
+					CR = "<CR> ",
+					Esc = "<Esc> ",
+					ScrollWheelDown = "<ScrollWheelDown> ",
+					ScrollWheelUp = "<ScrollWheelUp> ",
+					NL = "<NL> ",
+					BS = "<BS> ",
+					Space = "<Space> ",
+					Tab = "<Tab> ",
+					F1 = "<F1>",
+					F2 = "<F2>",
+					F3 = "<F3>",
+					F4 = "<F4>",
+					F5 = "<F5>",
+					F6 = "<F6>",
+					F7 = "<F7>",
+					F8 = "<F8>",
+					F9 = "<F9>",
+					F10 = "<F10>",
+					F11 = "<F11>",
+					F12 = "<F12>",
+				},
+			},
 
-      -- Document existing key chains
-      spec = {
-        { '<leader>f', group = '[F]iles' },
-        -- { '<leader>d', group = '[D]ocument' },
-        -- { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]indows' },
-        { '<leader>t', group = '[T]oggle' },
-        -- { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      },
-    },
-  }
+			-- Document existing key chains
+			spec = {
+				{ "<leader>b", group = "buffer" },
+				{ "<leader>d", group = "debug" },
+				{ "<leader>e", group = "errors" },
+				{ "<leader>f", group = "files" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>h", group = "help" },
+				{ "<leader>q", group = "quit" },
+				{ "<leader>r", group = "registers" },
+				{ "<leader>s", group = "search" },
+				{ "<leader>t", group = "toggle" },
+				{ "<leader>w", group = "windows" },
+				-- { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+			},
+		},
+	},
 }
