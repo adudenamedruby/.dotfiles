071cfc3004aec5c79b849270fe068f388aed319c
roux g. buciu <github@adudenamedruby.com>
2024-09-21 18:15:38 -0400

    Add lualine

---
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/lualine.lua
@@ -0,0 +1,72 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+
+		local function xcodebuild_device()
+			if vim.g.xcodebuild_platform == "macOS" then
+				return " macOS"
+			end
+
+			if vim.g.xcodebuild_os then
+				return " " .. vim.g.xcodebuild_device_name .. " (" .. vim.g.xcodebuild_os .. ")"
+			end
+
+			return " " .. vim.g.xcodebuild_device_name
+		end
+
+		lualine.setup({
+			options = {
+				globalstatus = true,
+				theme = "auto",
+				symbols = {
+					alternate_file = "#",
+					directory = "",
+					readonly = "",
+					unnamed = "[No Name]",
+					newfile = "[New]",
+				},
+				disabled_buftypes = { "quickfix", "prompt" },
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = {
+					-- { "mode" },
+					{ "filename" },
+				},
+				lualine_b = {
+					{ "diagnostics" },
+					{ "diff" },
+					{
+						"searchcount",
+						maxcount = 999,
+						timeout = 500,
+					},
+				},
+				lualine_c = {},
+				lualine_x = {
+					{ "' ' .. vim.g.xcodebuild_last_status", color = { fg = "#a6e3a1" } },
+					-- { "'󰙨 ' .. vim.g.xcodebuild_test_plan", color = { fg = "#a6e3a1", bg = "#161622" } },
+					{ xcodebuild_device, color = { fg = "#f9e2af", bg = "#161622" } },
+				},
+				lualine_y = {
+					{ "branch" },
+				},
+				lualine_z = {
+					{ "location" },
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			extensions = { "nvim-dap-ui", "quickfix", "trouble", "nvim-tree", "lazy", "mason" },
+		})
+	end,
+}
--- nvim/.config/nvim/lua/adudenamedruby/plugins/telescope.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/telescope.lua
@@ -67,15 +67,15 @@ return {
 
 		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>hs", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]iles [F]ind" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader>hs", builtin.help_tags, { desc = "search help" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "search keymaps" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find file" })
+		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "search Telescope builtin" })
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "search current word" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "grep search" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "diagnostics search" })
+		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "search resume" })
+		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = 'search recent files ("." for repeat)' })
 
 		vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "list all buffers" })
 
@@ -86,7 +86,7 @@ return {
 				winblend = 10,
 				previewer = false,
 			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
+		end, { desc = "fuzzy search in current buffer" })
 
 		-- It's also possible to pass additional configuration options.
 		--  See `:help telescope.builtin.live_grep()` for information about particular keys
@@ -95,11 +95,11 @@ return {
 				grep_open_files = true,
 				prompt_title = "Live Grep in Open Files",
 			})
-		end, { desc = "[S]earch [/] in Open Files" })
+		end, { desc = "search in open files" })
 
 		-- Shortcut for searching your Neovim configuration files
 		vim.keymap.set("n", "<leader>sn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+		end, { desc = "search NeoVim files" })
 	end,
 }
