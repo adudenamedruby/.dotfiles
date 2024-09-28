49b6c49c1804c3b2a3fa4de54489f8f2fd0594ee
roux g. buciu <github@adudenamedruby.com>
2024-09-28 19:11:07 -0400

    what's happening?

--- nvim/.config/nvim/lua/adudenamedruby/core/autocommands.lua
+++ nvim/.config/nvim/lua/adudenamedruby/core/autocommands.lua
@@ -2,9 +2,16 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+    pattern = "*NeogitStatus*",
+    callback = function()
+        vim.cmd("checktime")
+    end,
 })
