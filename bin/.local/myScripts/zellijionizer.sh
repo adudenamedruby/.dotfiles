037f04079327df63fba7d3cbd2515364ee2f394c
roux g. buciu <github@adudenamedruby.com>
2024-10-07 20:58:53 -0400

    more configs, but zellij continues to be a problem

--- bin/.local/myScripts/zellijionizer.sh
+++ bin/.local/myScripts/zellijionizer.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-directories="~/Developer/ ~/.dotfiles/zellij/.config/"
-selected=$(find $directories -mindepth 1 -maxdepth 1 -type d | fzf)
+zellij action resize increase left
+zellij action resize increase right
+directories=(~/Developer ~/.dotfiles/*/.config)
+selected=$(find "${directories[@]}" -mindepth 1 -maxdepth 1 -type d | fzf)
 
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-# selected_name=$(basename "$selected" | tr . _)
-# tmux_running=$(pgrep tmux)
-#
-# if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-#     tmux new-session -s $selected_name -c $selected
-#     exit 0
-# fi
-#
+selected_name=$(basename "$selected" | tr . _)
+
+zellij action detach
+zellij attach textbook
+
 # if ! tmux has-session -t=$selected_name 2>/dev/null; then
 #     tmux new-session -ds $selected_name -c $selected
 # fi
 #
 # tmux switch-client -t $selected_name
+sleep 10
