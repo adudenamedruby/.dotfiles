1b3924be2badf206435d94b3d903320a2ad981c7
roux g. buciu <github@adudenamedruby.com>
2024-11-17 21:26:24 -0500

    changes from theLab on Sun 17 Nov 2024 21:26:23 EST

--- nvim/.config/nvim/lua/adudenamedruby/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/nvim-cmp.lua
@@ -96,7 +96,7 @@ return {
                 -- Accept ([y]es) the completion.
                 --  This will auto-import if your LSP supports it.
                 --  This will expand snippets if the LSP sent a snippet.
-                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-t>"] = cmp.mapping.confirm({ select = true }),
 
                 -- If you prefer more traditional completion keymaps,
                 -- you can uncomment the following lines
