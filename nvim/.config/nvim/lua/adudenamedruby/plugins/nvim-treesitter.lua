3b3d798cc63c34cfe2490695ee3d8e7252a2315c
roux g. buciu <github@adudenamedruby.com>
2026-03-17 20:49:49 -0400

    update dotfiles

--- nvim/.config/nvim/lua/adudenamedruby/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            require("nvim-treesitter.config").setup({
                 ensure_installed = {
                     "bash",
                     "c",
