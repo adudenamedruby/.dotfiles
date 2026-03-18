1b6ade7f51f8c2f42af152c6349825c66e4d1a49
roux g. buciu <github@adudenamedruby.com>
2026-03-17 20:31:38 -0400

    update dotfiles

--- nvim/.config/nvim/lua/adudenamedruby/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/nvim-treesitter.lua
@@ -1,28 +1,9 @@
 return {
     {
-        -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        main = "treesitter.configs", -- Sets main module to use for opts
-        dependencies = {
-            "windwp/nvim-ts-autotag",
-        },
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
         config = function()
-            local treesitter = require("nvim-treesitter.configs")
-
-            ---@diagnostic disable-next-line: missing-fields
-            treesitter.setup({
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        node_incremental = "v",
-                        node_decremental = "V",
-                    },
-                },
-
-                autotag = { enable = false },
-
+            require("nvim-treesitter.configs").setup({
                 ensure_installed = {
                     "bash",
                     "c",
@@ -52,80 +33,57 @@ return {
 
                 auto_install = true,
 
-                highlight = {
+                -- Incremental selection (plugin module, no native equivalent)
+                incremental_selection = {
                     enable = true,
-                    -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-                    --  If you are experiencing weird indenting issues, add the language to
-                    --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-                    additional_vim_regex_highlighting = { "ruby" },
-                },
-
-                pickers = {
-                    find_files = {
-                        file_ignore_patterns = { "node_modules", ".git", ".venv" },
-                        hidden = true,
+                    keymaps = {
+                        node_incremental = "v",
+                        node_decremental = "V",
                     },
                 },
 
-                live_grep = {
-                    file_ignore_patterns = { "node_modules", ".git", ".venv" },
-                    additional_args = function(_)
-                        return { "--hidden" }
-                    end,
-                },
-
-                indent = { enable = true, disable = { "ruby" } },
-
+                -- Textobjects (configured here as required by nvim-treesitter-textobjects)
                 textobjects = {
                     select = {
                         enable = true,
-
-                        -- automatically jump forward to textobj, similar to targets.vim
                         lookahead = true,
-
                         keymaps = {
-                            -- you can use the capture groups defined in textobjects.scm
                             ["af"] = "@function.outer",
                             ["if"] = "@function.inner",
                             ["ac"] = "@class.outer",
-                            -- you can optionally set descriptions to the mappings (used in the desc parameter of
-                            -- nvim_buf_set_keymap) which plugins like which-key display
                             ["ic"] = { query = "@class.inner", desc = "select inner part of a class region" },
-                            -- you can also use captures from other query groups like `locals.scm`
                             ["as"] = { query = "@scope", query_group = "locals", desc = "select language scope" },
                         },
-                        -- you can choose the select mode (default is charwise 'v')
-                        --
-                        -- can also be a function which gets passed a table with the keys
-                        -- * query_string: eg '@function.inner'
-                        -- * method: eg 'v' or 'o'
-                        -- and should return the mode ('v', 'v', or '<c-v>') or a table
-                        -- mapping query_strings to modes.
                         selection_modes = {
-                            ["@parameter.outer"] = "v", -- charwise
-                            ["@function.outer"] = "v", -- linewise
-                            ["@class.outer"] = "<c-v>", -- blockwise
+                            ["@parameter.outer"] = "v",
+                            ["@function.outer"] = "v",
+                            ["@class.outer"] = "<c-v>",
                         },
-                        -- if you set this to `true` (default is `false`) then any textobject is
-                        -- extended to include preceding or succeeding whitespace. succeeding
-                        -- whitespace has priority in order to act similarly to eg the built-in
-                        -- `ap`.
-                        --
-                        -- can also be a function which gets passed a table with the keys
-                        -- * query_string: eg '@function.inner'
-                        -- * selection_mode: eg 'v'
-                        -- and should return true or false
                         include_surrounding_whitespace = true,
                     },
                 },
             })
+
+            -- Native treesitter highlighting (Neovim 0.11+)
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    if pcall(vim.treesitter.start) then
+                        -- Ruby needs vim regex highlighting alongside treesitter for indent rules
+                        if vim.bo.filetype == "ruby" then
+                            vim.bo.syntax = "on"
+                        end
+
+                        -- Native treesitter indent for all languages except Ruby
+                        if vim.bo.filetype ~= "ruby" then
+                            vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+                        end
+                    end
+                end,
+            })
         end,
-        -- There are additional nvim-treesitter modules that you can use to interact
-        -- with nvim-treesitter. You should go explore a few and see what interests you:
-        --
-        --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-        --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-        --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     },
-    { "nvim-treesitter/nvim-treesitter-textobjects" },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+    },
 }
