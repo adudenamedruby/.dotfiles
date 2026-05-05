77bc8d41da3692ed67f2be855773bfbdd434daca
roux g. buciu <github@adudenamedruby.com>
2026-05-05 16:46:05 -0400

    fix colourful menu

--- nvim/.config/nvim/lua/adudenamedruby/plugins/blink.lua
+++ nvim/.config/nvim/lua/adudenamedruby/plugins/blink.lua
@@ -330,33 +330,15 @@ return {
                 max_height = 6,
                 border = "none",
                 draw = {
+                    columns = { { "kind_icon" }, { "label", gap = 1 } },
                     components = {
                         label = {
                             width = { fill = true, max = 60 },
                             text = function(ctx)
-                                local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
-                                if highlights_info ~= nil then
-                                    return highlights_info.text
-                                else
-                                    return ctx.label
-                                end
+                                return require("colorful-menu").blink_components_text(ctx)
                             end,
                             highlight = function(ctx)
-                                local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
-                                local highlights = {}
-                                if highlights_info ~= nil then
-                                    for _, info in ipairs(highlights_info.highlights) do
-                                        table.insert(highlights, {
-                                            info.range[1],
-                                            info.range[2],
-                                            group = ctx.deprecated and "BlinkCmpLabelDeprecated" or info[1],
-                                        })
-                                    end
-                                end
-                                for _, idx in ipairs(ctx.label_matched_indices) do
-                                    table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-                                end
-                                return highlights
+                                return require("colorful-menu").blink_components_highlight(ctx)
                             end,
                         },
                     },
